4b469febe652bec6520d9326d4d26ba178fc4e59
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-02 21:42:11 +0000

    Set up project

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=> {
+  console.log("Server is running on port 3000");
+})
