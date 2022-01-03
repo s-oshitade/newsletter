6b1653786c1283a715bd4f66bb6dd56e939ac839
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-03 11:54:36 +0000

    Complete newsletter sign-in page

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const request = require('request');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=> {
+  res.sendFile(__dirname + '/signup.html');
+})
+
+app.post("/", (req, res)=>{
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+})
 app.listen(port, ()=> {
   console.log("Server is running on port 3000");
 })
+
