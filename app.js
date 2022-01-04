4da5303c93a73d893a14d594916db51d732913e7
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-04 11:42:30 +0000

    Incorporate APIs and prep for deployment on heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const https = require('https');
+// const request = require('request');
 const app = express();
 const port = 3000;
 
@@ -12,12 +13,55 @@ app.get("/", (req, res)=> {
 })
 
 app.post("/", (req, res)=>{
+
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
-  console.log(firstName, lastName, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName  
+        }
+    }
+  ]
+  };
+
+  let jsonData = JSON.stringify(data);
+
+  const url = "https://us20.api.mailchimp.com/3.0/lists/860e554851";
+  const options = {
+    method: "POST",
+    auth: "TLK:a015f0ea53e3e888f223b15dbc5960cd-us20"
+  }
+
+  const request = https.request(url, options, function(response){
+
+  if (response.statusCode === 200){
+    res.sendFile(__dirname + "/success.html");
+  } else {
+    res.sendFile(__dirname + "/failure.html");
+  }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+//  request.write(jsonData);
+ request.end();
+}) 
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
 })
-app.listen(port, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log("Server is running on port 3000");
 })
+
+// apiKey a015f0ea53e3e888f223b15dbc5960cd-us20
+// audienceID 860e554851
 
